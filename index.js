5aa70c0ff95b43e05856acb8e6538c917ac685b1
DoctorMuch <davidmueth@gmail.com>
2021-02-10 16:07:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Some programming languages (like Python) include a "title" method to return a string with Every Word Capitalized (e.g. 'title case'.title() returns 'Title Case').  
+
+JavaScript has no .title method, but that won't stop us!  Use the string methods you know to print 'Title Case' from 'title case'. */
+
+let notTitle = 'title case';
+
+console.log(notTitle.replace('t', 'T').replace('c', 'C'));
+
+console.log(notTitle[0].toUpperCase() + "itle " + notTitle[6].toUpperCase() + "ase");
